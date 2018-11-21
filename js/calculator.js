9a3780d0c8c050392c377af4b68fe3b340df0b36
Amanda Wright <amanda.jane.wright@gmail.com>
2018-11-21 13:55:17 +0000

    changing dropdowns to buttons for all packages

---
+++ js/calculator.js
@@ -0,0 +1,11 @@
+
+
+$("#wolf-month").click(function(event) {
+  var choice_data = parseInt(this.getAttribute("data-price"));
+}
+
+$("input").click(function(event) {
+  var choice_payment = this.getAttribute("data-payment");
+}
+
+function
