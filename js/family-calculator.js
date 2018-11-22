b6699faa62077f6e930e234443ca3b4a44affc65
Amanda Wright <amanda.jane.wright@gmail.com>
2018-11-22 09:37:35 +0000

    Moving calculators to individual files and finishing third calculator

---
+++ js/family-calculator.js
@@ -0,0 +1,48 @@
+var family_price = 0;
+var family_sim = '';
+var choice_payment_family = '';
+var total_family = 0;
+var total_inc_sims = 0;
+
+$(".family-gb-choice").click(function(event) {
+  family_price = parseInt(this.getAttribute("data-price"));
+});
+
+$(".sim-choice").click(function(event) {
+  family_sim = parseInt(this.getAttribute("data-sim"));
+});
+
+$(".family-payment-choice").click(function(event) {
+  choice_payment_family = this.getAttribute("value");
+});
+
+$(".calculate").click(function(event) {
+  calculate_total_family();
+  $("#total").text(total_family);
+});
+
+
+function calculate_total_inc_sims() {
+
+  if (family_price == "14") {
+    total_inc_sims = (family_price + ((family_sim - 1) * 7.2));
+  };
+  if (family_price == "20") {
+    total_inc_sims = (family_price + ((family_sim - 1) * 12))
+  };
+  if (family_price == "30") {
+    total_inc_sims = (family_price + ((family_sim - 1) * 20))
+  };
+  if (family_price == "40") {
+    total_inc_sims = (family_price + ((family_sim - 1) * 28))
+  };
+};
+
+function calculate_total_family() {
+ calculate_total_inc_sims();
+   total_family = total_inc_sims;
+  if (choice_payment_family == "annual-family") {
+    total_family = (total_inc_sims * 12);
+  };
+  return total_family;
+};
---
+++ js/wolf-calculator.js
@@ -0,0 +1,24 @@
+var wolf_price = 0;
+var choice_payment_wolf = '';
+var total_wolf = 0;
+
+$(".wolf-gb-choice").click(function(event) {
+  wolf_price = parseInt(this.getAttribute("data-price"));
+});
+
+$(".wolf-payment-choice").click(function(event) {
+  choice_payment_wolf = (this.getAttribute("value"));
+});
+
+$(".calculate-wolf").click(function(event) {
+  calculate_total_wolf();
+  $("#total-wolf").text(total_wolf);
+});
+
+function calculate_total_wolf() {
+  total_wolf = wolf_price;
+  if (choice_payment_wolf == "annual-lonewolf") {
+    total_wolf = (wolf_price * 12);
+  };
+  return total_wolf;
+};
